12f66f36bdbc9ee27257dc911cf364a4d295dfaa
n3212 <n3212@ndensan.co.jp>
2024-11-07 16:13:57 +0900

    実装完了

---
+++ BlazorApp/Components/Pages/Home.razor.cs
@@ -0,0 +1,89 @@
+﻿using BlazorApp.Models;
+
+namespace BlazorApp.Components.Pages;
+
+public partial class Home
+{
+    private List<Tasuku> list = default!;
+    bool sessionExsists => sessionState.HasState;
+
+    protected override void OnInitialized()
+    {
+        if (sessionExsists)
+        {
+            list = sessionState.State!;
+		}
+        else
+        {
+            SetInitialTask();
+
+		}
+		var c = new Comparison<Tasuku>(Compare);
+		list.Sort(c);
+	}
+
+    async Task LoadTask()
+    {
+        await Task.Yield();
+
+        if (sessionExsists)
+        {
+            sessionState.State = list;
+            return;
+        }
+    }
+
+    void SetInitialTask()
+    {
+        list ??= new List<Tasuku>
+        {
+            new Tasuku("タスク1", new DateTime(2024, 10, 31), 2, "ああああ"),
+            new Tasuku("タスク2", new DateTime(2024, 11, 30), 1, "いいいいいいい"),
+            new Tasuku("タスク3", new DateTime(2024, 12, 31), 0, "ううう")
+        };
+
+        if (!sessionExsists)
+        {
+            sessionState.State = list;
+        }
+    }
+
+	private void NavigateToTaskAddPage()
+    {
+        NavManager.NavigateTo("taskadd");
+	}
+
+	static string GetState(int state)
+	{
+		switch (state)
+		{
+			case 0:
+				return "未着手";
+			case 1:
+				return "仕掛中";
+			case 2:
+				return "完了";
+			case 9:
+				return "無視";
+			default:
+				return string.Empty;
+		}
+    }
+
+    static int Compare(Tasuku a, Tasuku b)
+	{
+        if (a.State != b.State)
+        {
+			return a.State - b.State;
+        }
+
+        if( a.Deadline >= b.Deadline)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
---
+++ BlazorApp/Models/Tasuku.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models;
+
+public class Tasuku(string title, DateTime deadline, int state, string contents)
+{
+	[Required(ErrorMessage = "題名を入力してください。")]
+	public string Title { get; set; } = title;
+
+	[Required(ErrorMessage = "期限を入力してください。")]
+	public DateTime Deadline { get; set; } = deadline;
+
+	[Required(ErrorMessage = "状態を入力してください。")]
+	public int State { get; set; } = state;
+
+	public string Contents { get; set; } = contents;
+}
